317c28a0ddf725800cd5bda7ec4f861d5f519408
H <git@hafio.tv>
2025-09-17 11:44:42 +0800

    added more show lines

--- 069-gather-configs.sh
+++ 069-gather-configs.sh
@@ -52,105 +52,112 @@ no paging
 
 ! some commands for specific to appliance vs software
 
-show acl-profile * > configs/.out/show-aclprofiles.out
-show acl-profile * detail > configs/.out/show-aclprofiles-detail.out
-show alarm > configs/.out/show-alarm.out
-show authentication > configs/.out/show-auth.out
-show authentication access-level > configs/.out/show-auth-access-level.out
-show authentication access-level detail > configs/.out/show-auth-access-level-detail.out
-show backup > configs/.out/show-backup.out
-show bridge * > configs/.out/show-bridges.out
-show bridge * detail > configs/.out/show-bridges-detail.out
-show bridge * stats > configs/.out/show-bridge-stats.out
-show bridge * stats queues > configs/.out/show-bridge-stats-queues.out
-show cache-cluster * detail > configs/.out/show-cachecluster.out
-show cache-instance * detail > configs/.out/show-cacheinstance.out
-show client * > configs/.out/show-clients.out
-show client * detail > configs/.out/show-clients-detail.out
-show client-profile * > configs/.out/show-clientprofile.out
-show client-profile * detail > configs/.out/show-clientprofile-detail.out
-show client-username * > configs/.out/show-client-username.out
-show client-username * detail > configs/.out/show-client-username-detail.out
-show clock detail > configs/.out/show-clock-detail.out
-show cluster * detail > configs/.out/show-cluster-detail.out
-show cluster * link * detail > configs/.out/show-cluster-link-detail.out
-show compression > configs/.out/show-compression.out
-show config-sync > configs/.out/show-config-sync.out
-show config-sync database > configs/.out/show-config-sync-database.out
-show config-sync database detail > configs/.out/show-config-sync-database-detail.out
-show cspf stats > configs/.out/show-cspf-stats.out
-show current-config all > configs/.out/show-currentconfig-all.out
-show current-config message-vpn * > configs/.out/show-currentconfig-vpns.out
-show debug lldp > configs/.out/show-debug-lldp.out
-show disk > configs/.out/show-disk.out
-show disk detail > configs/.out/show-disk-detail.out
-show distributed-cache * detail > configs/.out/show-distributedcache.out
-show dns > configs/.out/show-dns.out
-show hardware details > configs/.out/show-hardware-details.out
-show hardware post > configs/.out/show-hardware-post.out
-show hostname > configs/.out/show-hostname.out
-show interface detail > configs/.out/show-interface-detail.out
-show ip vrf management > configs/.out/show-vrf-mgmt.out
-show ip vrf msg-backbone > configs/.out/show-vrf-msg-backbone.out
-show jndi connection-factory * detail > configs/.out/show-jndi-cf.out
-show jndi queue * detail > configs/.out/show-jndi-queues.out
-show jndi summary > configs/.out/show-jndi-summary.out
-show jndi topic * detail > configs/.out/show-jndi-topics.out
-show kerberos keytab > configs/.out/show-kerberose-keytab.out
-show kerberos keytab detail > configs/.out/show-kerberose-keytab-details.out
-show ldap-profile * detail > configs/.out/show-ldap-profile-detail.out
-show memory > configs/.out/show-memory.out
-show message-spool detail > configs/.out/show-message-spool-detail.out
-show message-spool message-vpn * detail > configs/.out/show-message-spool-vpn-detail.out
-show message-spool rates > configs/.out/show-message-spool-rates.out
-show message-spool stats > configs/.out/show-message-spool-stats.out
-show message-vpn * > configs/.out/show-vpns.out
-show message-vpn * detail > configs/.out/show-vpn-details.out
-show message-vpn * dynamic-message-routing > configs/.out/show-vpn-dmr.out
-show message-vpn * dynamic-message-routing dmr-bridge * > configs/.out/show-vpn-dmr-bridge.out
-show message-vpn * mqtt > configs/.out/show-vpn-mqtt.out
-show message-vpn * mqtt mqtt-session * > configs/.out/show-vpn-mqtt-session.out
-show message-vpn * mqtt retain cache * > configs/.out/show-vpn-mqtt-retain-cache.out
-show message-vpn * replication > configs/.out/show-vpns-replication.out
-show message-vpn * rest > configs/.out/show-vpn-rest.out
-show message-vpn * rest rest-delivery-point * detail > configs/.out/show-vpn-rdp-detail.out
-show message-vpn * service > configs/.out/show-vpn-service.out
-show mqtt > configs/.out/show-mqtt.out
-show oauth-profile * detail > configs/.out/show-oauth-profile-detail.out
-show product-key > configs/.out/show-product-key.out
-show queue * > configs/.out/show-queues.out
-show queue * detail > configs/.out/show-queues-details.out
-show redundancy > configs/.out/show-redundancy.out
-show redundancy detail > configs/.out/show-redundancy-detail.out
-show redundancy group > configs/.out/show-redundancy-group.out
-show replay-log * > configs/.out/show-replay-log.out
-show replicated-topic * > configs/.out/show-replicated-topics.out
-show replication > configs/.out/show-replication.out
-show router-name > configs/.out/show-routername.out
-show routing > configs/.out/show-routing.out
-show service > configs/.out/show-service.out
-show service semp > configs/.out/show-service.semp.out
-show service virtual-hostname * > configs/.out/show-service-virtual-hostname.out
-show service web-transport > configs/.out/show-service-web-transport.out
-show snmp > configs/.out/show-snmp.out
-show snmp trap * > configs/.out/show-snmp-trap.out
-show ssl allow-tls-version > configs/.out/show-ssl-allowed-tls.out
-show ssl certificate-files > configs/.out/show-ssl-certificate-files.out
-show ssl cipher-suite-list default > configs/.out/show-ssl-cipher-default.out
-show ssl cipher-suite-list management > configs/.out/show-ssl-cipher-management.out
-show ssl cipher-suite-list msg-backbone > configs/.out/show-ssl-cipher-msg-backbone.out
-show ssl cipher-suite-list ssh > configs/.out/show-ssl-cipher-ssh.out
-show ssl server-certificate > configs/.out/show-ssl-server-certificate.out
-show ssl server-certificate detail > configs/.out/show-ssl-server-certificate-detail.out
-show syslog > configs/.out/show-syslog.out
-show system detail > configs/.out/show-system.out
-show system health > configs/.out/show-system-health.out
-show telemetry > configs/.out/show-telemetry.out
-show topic-endpoint * detail > configs/.out/show-topicendpoints.out
-show username * > configs/.out/show-username.out
-show username * detail > configs/.out/show-username-detail.out
-show version > configs/.out/show-version.out
-show web-manager > configs/.out/show-web-manager.out
+show acl-profile * > configs/cliout/show-aclprofiles.out
+show acl-profile * detail > configs/cliout/show-aclprofiles-detail.out
+show alarm > configs/cliout/show-alarm.out
+show authentication > configs/cliout/show-auth.out
+show authentication access-level > configs/cliout/show-auth-access-level.out
+show authentication access-level detail > configs/cliout/show-auth-access-level-detail.out
+show backup > configs/cliout/show-backup.out
+show bridge * > configs/cliout/show-bridges.out
+show bridge * detail > configs/cliout/show-bridges-detail.out
+show bridge * stats > configs/cliout/show-bridge-stats.out
+show bridge * stats queues > configs/cliout/show-bridge-stats-queues.out
+show cache-cluster * detail > configs/cliout/show-cachecluster.out
+show cache-instance * detail > configs/cliout/show-cacheinstance.out
+show client * > configs/cliout/show-clients.out
+show client * detail > configs/cliout/show-clients-detail.out
+show client-certificate-authority ca-name * cert > configs/cliout/show-client-cert-auth-cert.out
+show client-certificate-authority ca-name * detail > configs/cliout/show-client-cert-auth-detail.out
+show client-profile * > configs/cliout/show-clientprofile.out
+show client-profile * detail > configs/cliout/show-clientprofile-detail.out
+show client-username * > configs/cliout/show-client-username.out
+show client-username * detail > configs/cliout/show-client-username-detail.out
+show clock detail > configs/cliout/show-clock-detail.out
+show cluster * > configs/cliout/show-cluster.out
+show cluster * detail > configs/cliout/show-cluster-detail.out
+show cluster * link * detail > configs/cliout/show-cluster-link-detail.out
+show compression > configs/cliout/show-compression.out
+show config-sync > configs/cliout/show-config-sync.out
+show config-sync database > configs/cliout/show-config-sync-database.out
+show config-sync database detail > configs/cliout/show-config-sync-database-detail.out
+show cspf stats > configs/cliout/show-cspf-stats.out
+show current-config all > configs/cliout/show-currentconfig-all.out
+show current-config message-vpn * > configs/cliout/show-currentconfig-vpns.out
+show debug lldp > configs/cliout/show-debug-lldp.out
+show disk > configs/cliout/show-disk.out
+show disk detail > configs/cliout/show-disk-detail.out
+show distributed-cache * detail > configs/cliout/show-distributedcache.out
+show dns > configs/cliout/show-dns.out
+show domain-certificate-authority ca-name * cert > configs/cliout/show-domain-cert-auth.out
+show hardware details > configs/cliout/show-hardware-details.out
+show hardware post > configs/cliout/show-hardware-post.out
+show hostname > configs/cliout/show-hostname.out
+show interface detail > configs/cliout/show-interface-detail.out
+show ip vrf management > configs/cliout/show-vrf-mgmt.out
+show ip vrf msg-backbone > configs/cliout/show-vrf-msg-backbone.out
+show jndi connection-factory * detail > configs/cliout/show-jndi-cf.out
+show jndi queue * detail > configs/cliout/show-jndi-queues.out
+show jndi summary > configs/cliout/show-jndi-summary.out
+show jndi topic * detail > configs/cliout/show-jndi-topics.out
+show kerberos keytab > configs/cliout/show-kerberose-keytab.out
+show kerberos keytab detail > configs/cliout/show-kerberose-keytab-details.out
+show ldap-profile * detail > configs/cliout/show-ldap-profile-detail.out
+show memory > configs/cliout/show-memory.out
+show message-spool detail > configs/cliout/show-message-spool-detail.out
+show message-spool message-vpn * detail > configs/cliout/show-message-spool-vpn-detail.out
+show message-spool rates > configs/cliout/show-message-spool-rates.out
+show message-spool stats > configs/cliout/show-message-spool-stats.out
+show message-vpn * > configs/cliout/show-vpns.out
+show message-vpn * authorization > configs/cliout/show-vpn-auth.out
+show message-vpn * authorization authorization-group * > configs/cliout/show-vpn-auth-authgroup.out
+show message-vpn * detail > configs/cliout/show-vpn-details.out
+show message-vpn * dynamic-message-routing > configs/cliout/show-vpn-dmr.out
+show message-vpn * dynamic-message-routing dmr-bridge * > configs/cliout/show-vpn-dmr-bridge.out
+show message-vpn * mqtt > configs/cliout/show-vpn-mqtt.out
+show message-vpn * mqtt mqtt-session * > configs/cliout/show-vpn-mqtt-session.out
+show message-vpn * mqtt retain cache * > configs/cliout/show-vpn-mqtt-retain-cache.out
+show message-vpn * replication > configs/cliout/show-vpns-replication.out
+show message-vpn * rest > configs/cliout/show-vpn-rest.out
+show message-vpn * rest rest-delivery-point * detail > configs/cliout/show-vpn-rdp-detail.out
+show message-vpn * service > configs/cliout/show-vpn-service.out
+show mqtt > configs/cliout/show-mqtt.out
+show oauth-profile * detail > configs/cliout/show-oauth-profile-detail.out
+show product-key > configs/cliout/show-product-key.out
+show queue * > configs/cliout/show-queues.out
+show queue * detail > configs/cliout/show-queues-details.out
+show redundancy > configs/cliout/show-redundancy.out
+show redundancy detail > configs/cliout/show-redundancy-detail.out
+show redundancy group > configs/cliout/show-redundancy-group.out
+show replay-log * > configs/cliout/show-replay-log.out
+show replicated-topic * > configs/cliout/show-replicated-topics.out
+show replication > configs/cliout/show-replication.out
+show router-name > configs/cliout/show-routername.out
+show routing > configs/cliout/show-routing.out
+show service > configs/cliout/show-service.out
+show service semp > configs/cliout/show-service.semp.out
+show service virtual-hostname * > configs/cliout/show-service-virtual-hostname.out
+show service web-transport > configs/cliout/show-service-web-transport.out
+show snmp > configs/cliout/show-snmp.out
+show snmp trap * > configs/cliout/show-snmp-trap.out
+show ssl allow-tls-version > configs/cliout/show-ssl-allowed-tls.out
+show ssl certificate-files > configs/cliout/show-ssl-certificate-files.out
+show ssl cipher-suite-list default > configs/cliout/show-ssl-cipher-default.out
+show ssl cipher-suite-list management > configs/cliout/show-ssl-cipher-management.out
+show ssl cipher-suite-list msg-backbone > configs/cliout/show-ssl-cipher-msg-backbone.out
+show ssl cipher-suite-list ssh > configs/cliout/show-ssl-cipher-ssh.out
+show ssl server-certificate > configs/cliout/show-ssl-server-certificate.out
+show ssl server-certificate detail > configs/cliout/show-ssl-server-certificate-detail.out
+show syslog > configs/cliout/show-syslog.out
+show system post > configs/cliout/show-system-post.out
+show system detail > configs/cliout/show-system.out
+show system health > configs/cliout/show-system-health.out
+show telemetry > configs/cliout/show-telemetry.out
+show topic-endpoint * detail > configs/cliout/show-topicendpoints.out
+show username * > configs/cliout/show-username.out
+show username * detail > configs/cliout/show-username-detail.out
+show version > configs/cliout/show-version.out
+show web-manager > configs/cliout/show-web-manager.out
 
 ! gather diagnostics '${DAYS}' days
 end
