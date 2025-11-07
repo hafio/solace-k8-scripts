d0cc35b1068dc202b6e73596d5420e12c6e0184e
H <git@hafio.tv>
2025-11-07 10:25:55 +0800

    added --keep-pvc option

--- 120-delete-broker.sh
+++ 120-delete-broker.sh
@@ -8,6 +8,13 @@ else
 	exit 1
 fi
 
+if [[ "${1:0:1}" == "?" ]] || [[ "${1:0:2}" == "-h" ]] || [[ "${1:0:3}" == "--h" ]]; then
+	echo "Usage: $0 [OPTIONS]"
+	echo "	OPTIONS:"
+  echo "    --keep-pvc: does not remove Persistent Volume Claims of the broker."
+	exit
+fi
+
 TMPFILE=.tmp-${BASHPID}
 
 echo "apiVersion: pubsubplus.solace.com/v1beta1
@@ -18,11 +25,25 @@ metadata:
 " > ${TMPFILE}
 
 ${KUBE} delete -f ${TMPFILE}
-
-echo "Deleting PVCs..."
-${KUBE} delete pvc -n ${SOLBK_NS} data-${SOLBK_NAME}-pubsubplus-p-0 2> /dev/null
-[[ "${SOLBK_REDUNDANCY:-false}" == true ]] && (
-	${KUBE} delete pvc -n ${SOLBK_NS} data-${SOLBK_NAME}-pubsubplus-b-0 2> /dev/null
-	${KUBE} delete pvc -n ${SOLBK_NS} data-${SOLBK_NAME}-pubsubplus-m-0 2> /dev/null
-)
 rm ${TMPFILE}
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		--keep-pvc)
+			KEEP=true
+			shift 1
+			;;
+		*)
+			shift
+			;;
+	esac
+done
+
+if [[ "${KEEP}" != "true" ]]; then
+  echo "Deleting PVCs..."
+  ${KUBE} delete pvc -n ${SOLBK_NS} data-${SOLBK_NAME}-pubsubplus-p-0 2> /dev/null
+  [[ "${SOLBK_REDUNDANCY:-false}" == true ]] && (
+    ${KUBE} delete pvc -n ${SOLBK_NS} data-${SOLBK_NAME}-pubsubplus-b-0 2> /dev/null
+    ${KUBE} delete pvc -n ${SOLBK_NS} data-${SOLBK_NAME}-pubsubplus-m-0 2> /dev/null
+  )
+fi
